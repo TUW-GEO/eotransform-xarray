f83e6677cf976c5899d8531cede773516c8a8794
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-08-10 15:35:24 +0200

    e use project tokens to access eotransform-pandas pypi

--- ci/add-pypi-indices.sh
+++ ci/add-pypi-indices.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TOKEN=$1
+EOTRANSFORM_TOKEN=$1
+EOTRANSFORM_PANDAS_TOKEN=$2
 
 if [ -f ~/.config/pip/pip.conf ]; then
 	echo "pip.conf already exists, you've to add python indices manually."
@@ -10,7 +11,7 @@ else
 	cat <<- EOF > ~/.config/pip/pip.conf
 	[global]
 	extra-index-url =
-	  https://__token__:${TOKEN}@git.eodc.eu/api/v4/projects/717/packages/pypi/simple
-	  https://__token__:${TOKEN}@git.eodc.eu/api/v4/projects/719/packages/pypi/simple
+	  https://__token__:${EOTRANSFORM_TOKEN}@git.eodc.eu/api/v4/projects/717/packages/pypi/simple
+	  https://__token__:${EOTRANSFORM_PANDAS_TOKEN}@git.eodc.eu/api/v4/projects/719/packages/pypi/simple
 	EOF
 fi
