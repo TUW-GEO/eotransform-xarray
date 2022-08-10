97b4d061fb65cc6baa05c187f11994d010d8d5ec
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-08-10 07:53:14 +0200

    F!! RandomSample transformer

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import gc
+import random
 import sys
 from pathlib import Path
 
+import numpy as np
 import pytest
 
 sys.path.append((Path(__file__).parent / "helpers").as_posix())
@@ -14,3 +16,14 @@ def disabled_gc():
         yield
     finally:
         gc.enable()
+
+
+@pytest.fixture(scope='module')
+def fixed_seed():
+    py_state = random.getstate()
+    np_state = np.random.get_state()
+    random.seed(42)
+    np.random.seed(42)
+    yield 42
+    random.setstate(py_state)
+    np.random.set_state(np_state)
