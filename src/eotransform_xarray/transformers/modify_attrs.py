01a8f9ea3367da6b7a3d3484892593e20b910039
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-09-07 13:01:18 +0200

    d add ModifyAttrs doc

--- src/eotransform_xarray/transformers/modify_attrs.py
+++ src/eotransform_xarray/transformers/modify_attrs.py
@@ -4,7 +4,20 @@ from eotransform_xarray.transformers import TransformerOfXArrayData, XArrayData
 
 
 class ModifyAttrs(TransformerOfXArrayData):
+    """
+    Modify the attributes of a xarray DataArray or Dataset with the given function
+    >>> from xarray import DataArray
+    >>> def add_foo_bar(attrs):
+    ...     attrs['foo'] = "bar"
+    ...     return attrs
+    >>> ModifyAttrs(add_foo_bar)(DataArray([0], attrs=dict(initial='attribute'))).attrs
+    {'initial': 'attribute', 'foo': 'bar'}
+    """
+
     def __init__(self, modification_fn: Callable[[Dict], Dict]):
+        """
+        :param modification_fn: function which is used to modify the attributes of the Dataset or DataArray
+        """
         self._modification_fn = modification_fn
 
     def __call__(self, x: XArrayData) -> XArrayData:
