5069e6b99a082a2e47030e5e8f2b171d5cccef52
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-10-19 10:11:13 +0200

    t test look up radius

--- tests/test_transform_resample_with_gauss.py
+++ tests/test_transform_resample_with_gauss.py
@@ -10,6 +10,7 @@ from pytest_approvaltests_geo import GeoOptions
 from xarray import DataArray
 
 from eotransform_xarray.transformers.resample_with_gauss import Swath, Extent, Area, ResampleWithGauss
+from helpers.assertions import assert_raster_eq, assert_data_array_eq
 
 DEFAULT_TEST_EXTENT = Extent(4800000, 1200000, 5400000, 1800000)
 DEFAULT_TEST_TRANSFORM = Affine.from_gdal(4800000, 3000, 0, 1800000, 0, 3000)
@@ -41,10 +42,11 @@ def make_target_area(columns: int, rows: int) -> Area:
 
 
 def make_swath_data_array(values: Any, swath: Swath) -> DataArray:
+    values = np.array(values)
     return DataArray(values, dims=['time', 'parameter', 'value'], coords=(dict(
-        time=np.arange(0, 2),
-        lon=(('time', 'parameter', 'value'), np.stack([swath.lons, swath.lons])),
-        lat=(('time', 'parameter', 'value'), np.stack([swath.lats, swath.lats])))))
+        time=np.arange(0, values.shape[0]),
+        lon=(('time', 'parameter', 'value'), np.tile(swath.lons, (*values.shape[:-1], 1))),
+        lat=(('time', 'parameter', 'value'), np.tile(swath.lats, (*values.shape[:-1], 1))))))
 
 
 def mask_and_scale(a: DataArray) -> DataArray:
@@ -54,3 +56,15 @@ def mask_and_scale(a: DataArray) -> DataArray:
     a = a.fillna(-9999)
     a.rio.write_nodata(-9999, inplace=True)
     return a.astype(np.int16)
+
+
+def test_resample_raster_with_gauss_uses_max_lookup_radius(verify_raster_as_geo_tif):
+    swath = make_swath([12.0, 16.0], [47.9, 45.2])
+    swath.lons[0, -1] = 21.5
+    swath.lats[0, -1] = 40.5
+    in_data = make_swath_data_array([[[1, 2, 4, 8], [1, 2, 4, np.nan]]], swath)
+
+    resample = ResampleWithGauss(swath, make_target_area(200, 200), sigma=2e5, neighbours=4, lookup_radius=5e5)
+    resampled = resample(in_data)
+
+    assert_data_array_eq(resampled[0, 0], resampled[0, 1])
