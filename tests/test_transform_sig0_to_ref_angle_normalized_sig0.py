919048feb6be8929f4a30b11d087aa4fac3ebdd6
Berhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-01-30 16:38:18 +0100

    t test that everything is masked properly

--- tests/test_transform_sig0_to_ref_angle_normalized_sig0.py
+++ tests/test_transform_sig0_to_ref_angle_normalized_sig0.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import numpy as np
 from xarray import DataArray
 
 from assertions import assert_raster_eq
@@ -25,3 +26,16 @@ class NormalizeSig0ToRefLiaBySlope(TransformerOfDataArray):
 def test_sig0_is_interpolated_to_reference_angle_based_on_slope():
     normalize = NormalizeSig0ToRefLiaBySlope(make_raster([[-0.1]]), make_raster([[20]]), Degree(40))
     assert_raster_eq(normalize(make_raster([[-10]])), make_raster([[-12]]))
+
+
+def test_mask_all_values_where_slope_plia_and_sig0_are_nan():
+    normalize = NormalizeSig0ToRefLiaBySlope(
+        make_raster([[np.nan, -0.1],
+                     [-0.1, -0.1]]),
+        make_raster([[20, np.nan],
+                     [20, 20]]), Degree(40))
+    assert_raster_eq(normalize(
+        make_raster([[-10, -10],
+                     [np.nan, -10]])),
+        make_raster([[np.nan, np.nan],
+                     [np.nan, -12]]))
