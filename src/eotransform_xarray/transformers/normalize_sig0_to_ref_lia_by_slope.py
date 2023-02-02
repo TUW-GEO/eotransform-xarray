e9fcc044ae9fdd7de51ddae23c613c5bd43e831b
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-02-02 15:21:31 +0100

    F!! specify LIA angles for multiple orbits for normalization

--- src/eotransform_xarray/transformers/normalize_sig0_to_ref_lia_by_slope.py
+++ src/eotransform_xarray/transformers/normalize_sig0_to_ref_lia_by_slope.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from typing import Optional
+from typing import Optional, Dict
 
 import numpy as np
 from xarray import DataArray
@@ -8,6 +8,8 @@ from eotransform_xarray.geometry.degrees import Degree
 from eotransform_xarray.numba_engine.normalize_sig0_to_ref_lia_by_slope import normalize_numba
 from eotransform_xarray.transformers import TransformerOfDataArray
 
+ORBIT_KEY = 'orbit'
+
 
 class Engine(Enum):
     DASK = auto()
@@ -15,16 +17,19 @@ class Engine(Enum):
 
 
 class NormalizeSig0ToRefLiaBySlope(TransformerOfDataArray):
-    def __init__(self, slope: DataArray, lia: DataArray, reference_lia: Degree, engine: Optional[Engine] = Engine.DASK):
+    def __init__(self, slope: DataArray, lias_per_orbit: Dict[str, DataArray], reference_lia: Degree,
+                 engine: Optional[Engine] = Engine.DASK):
         self._slope = slope
-        self._lia = lia
+        self._lias_per_orbit = lias_per_orbit
         self._reference_lia = reference_lia
         self._engine = engine
 
     def __call__(self, x: DataArray) -> DataArray:
+        lia = self._lias_per_orbit[x.attrs[ORBIT_KEY]]
         if self._engine == Engine.DASK:
-            return x - self._slope * (self._lia - self._reference_lia.value)
+            return x - self._slope * (lia - self._reference_lia.value)
         if self._engine == Engine.NUMBA:
             out = np.empty_like(x.values)
-            normalize_numba(x.values, self._slope.values, self._lia.values, self._reference_lia.value, out)
+            normalize_numba(x.values, self._slope.values, lia.values, self._reference_lia.value,
+                            out)
             return x.copy(data=out)
--- tests/test_transform_sig0_to_ref_angle_normalized_sig0.py
+++ tests/test_transform_sig0_to_ref_angle_normalized_sig0.py
@@ -3,14 +3,19 @@ import pytest
 
 from assertions import assert_raster_eq
 from eotransform_xarray.geometry.degrees import Degree
-from eotransform_xarray.transformers.normalize_sig0_to_ref_lia_by_slope import NormalizeSig0ToRefLiaBySlope, Engine
+from eotransform_xarray.transformers.normalize_sig0_to_ref_lia_by_slope import NormalizeSig0ToRefLiaBySlope, Engine, \
+    ORBIT_KEY
 from helpers.factories import make_raster
 
 
 @pytest.mark.parametrize('engine', [Engine.DASK, Engine.NUMBA])
 def test_sig0_is_interpolated_to_reference_angle_based_on_slope(engine):
-    normalize = NormalizeSig0ToRefLiaBySlope(make_raster([[-0.1]]), make_raster([[20]]), Degree(40), engine)
-    assert_raster_eq(normalize(make_raster([[-10]])), make_raster([[-12]]))
+    normalize = NormalizeSig0ToRefLiaBySlope(make_raster([[-0.1]]), {
+        '095': make_raster([[20]]),
+        '015': make_raster([[10]]),
+    }, Degree(40), engine)
+    assert_raster_eq(normalize(make_raster([[-10]], attrs={ORBIT_KEY: '095'})), make_raster([[-12]]))
+    assert_raster_eq(normalize(make_raster([[-10]], attrs={ORBIT_KEY: '015'})), make_raster([[-13]]))
 
 
 @pytest.mark.parametrize('engine', [Engine.DASK, Engine.NUMBA])
@@ -18,10 +23,10 @@ def test_mask_all_values_where_slope_plia_and_sig0_are_nan(engine):
     normalize = NormalizeSig0ToRefLiaBySlope(
         make_raster([[np.nan, -0.1],
                      [-0.1, -0.1]]),
-        make_raster([[20, np.nan],
-                     [20, 20]]), Degree(40), engine)
+        {'095': make_raster([[20, np.nan],
+                             [20, 20]])}, Degree(40), engine)
     assert_raster_eq(normalize(
         make_raster([[-10, -10],
-                     [np.nan, -10]])),
+                     [np.nan, -10]], attrs={ORBIT_KEY: '095'})),
         make_raster([[np.nan, np.nan],
                      [np.nan, -12]]))
